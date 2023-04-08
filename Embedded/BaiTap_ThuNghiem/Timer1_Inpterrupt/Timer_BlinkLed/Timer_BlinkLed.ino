1de73f7f82239f746ce4a57a96cb3b3b512fc7a6
Dev-LinhPham <87310137+Dev-LinhPham@users.noreply.github.com>
2023-04-08 08:27:04 +0700

    Add comment

--- Embedded/BaiTap_ThuNghiem/Timer1_Inpterrupt/Timer_BlinkLed/Timer_BlinkLed.ino
+++ Embedded/BaiTap_ThuNghiem/Timer1_Inpterrupt/Timer_BlinkLed/Timer_BlinkLed.ino
@@ -1,32 +1,60 @@
-// Ví dụ này dùng ngắt Timer để nháy LED
-// và sử dụng biến chia sẻ giữa hàm ngắt và chương trình chính
+/*
+* File: Timer_BlinkLed.c
+* Author: Linh Pham
+* Date: 05/04/2023
+* Description: Sử dụng ngắt Timer để BlinkLed
+*/
 #include <TimerOne.h>
 
-byte ledPin[] = {2, 3, 4, 5, 6, 7, 8, 9};
-byte sizeLed;
+byte ledPin[] = {2, 3, 4, 5, 6, 7, 8, 9};// Mảng các chân sử dụng
+byte sizeLedPin;
 int stateLed = LOW;
 
+/*
+* Function: setup
+* Description: Hàm chỉ chạy 1 lần duy nhất khi Arduino hoạt động
+* Input:
+*   void
+* Output:
+*   return: None
+*/
 void setup(void)
 {
   Timer1.initialize(150000); //Khởi động ngắt, thời gian đặt cho nó là 150000us=0.15s.
   Timer1.attachInterrupt(blinkLED); //Khi xảy ra ngắt chương trình sẽ gọi hàm blinkLED().
   Serial.begin(9600);
-  sizeLed = sizeof(ledPin);
-  for (int i = 0; i < sizeLed; i++) {
+  sizeLedPin = sizeof(ledPin);
+  for (int i = 0; i < sizeLedPin; i++) {
     pinMode(ledPin[i], OUTPUT);
     digitalWrite(ledPin[i], stateLed);
   }
 }
 
+/*
+* Function: blinkLED
+* Description: Hàm nhấp nháy Led khi ngắt Timer xảy ra
+* Input:
+*   void
+* Output:
+*   return: None
+*/
 void blinkLED(void)
 {
   stateLed = !stateLed;
-  for (int j = 0; j < sizeLed; j++) {
+  for (int j = 0; j < sizeLedPin; j++) {
     digitalWrite(ledPin[j], stateLed);
   }
   delay(50);
 }
 
+/*
+* Function: loop
+* Description: Hàm nhấp nháy Led khi ngắt Timer xảy ra
+* Input:
+*   void
+* Output:
+*   return: None
+*/
 void loop(void)
 {
   delay(100);
--- Embedded/BaiTap_ThuNghiem/Timer1_PWM/LedPWM/LedPWM.ino
+++ Embedded/BaiTap_ThuNghiem/Timer1_PWM/LedPWM/LedPWM.ino
@@ -1,15 +1,35 @@
+/*
+* File: LedPWM.c
+* Author: Linh Pham
+* Date: 05/04/2023
+* Description: Sử dụng Timer1 để băm xung PWM điều khiển độ sáng đèn Led
+*/
 #include <TimerOne.h>
-
-/* Sử dụng Timer1 để băm xung PWM điều khiển độ sáng đèn Led*/
 
 const int ledPin = 10;
 
+/*
+* Function: setup
+* Description: Hàm chỉ chạy 1 lần duy nhất khi Arduino hoạt động
+* Input:
+*   void
+* Output:
+*   return: None
+*/
 void setup(void)
 {
   Timer1.initialize(25000);  // 50000us = 0.05s
   Serial.begin(9600);
 }
 
+/*
+* Function: loop
+* Description: Hàm chạy lặp đi lặp lại khi Arduino hoạt động
+* Input:
+*   void
+* Output:
+*   return: None
+*/
 void loop(void)
 {
   // slowly increase the PWM led
@@ -17,7 +37,7 @@ void loop(void)
   for (int dutyCycle = 0; dutyCycle < 1023; dutyCycle++) {
     Serial.print("PWM Led, Duty Cycle = ");
     Serial.println(dutyCycle);
-    Timer1.pwm(ledPin, dutyCycle);
+    Timer1.pwm(ledPin, dutyCycle);// Băm xung PWM 
     delay(100);
   }
 }
